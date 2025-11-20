b12118d8c7baa6b467b04e4b2f15b74337fb78ac
Isaak Vogel <isaak.vogel@student.htldornbirn.at>
2025-11-20 13:27:56 +0100

    fix 30.js

--- js/1bWI_Basics/30_Variablen_Aufgaben/30.js
+++ js/1bWI_Basics/30_Variablen_Aufgaben/30.js
@@ -23,8 +23,8 @@ var applePricePerKilo = 3.43;
  
 var roundedResult1 = Math.round(0.34 * applePricePerKilo * 8);
 var roundedResult2 = Math.round(0.22 * bananaPricePerKilo * 17);
-var roundedResult3 = Math.round(0.34 * applePricePerKilo * 1000);
-var roundedResult4 = Math.round(0.22 * bananaPricePerKilo * 1000);
+var roundedResult3 = Math.round(applePricePerKilo * 1000);
+var roundedResult4 = Math.round(bananaPricePerKilo * 1000);
 
 console.log("8 Äpfel: " + roundedResult1);
 console.log("17 Bananen: " + roundedResult2);
