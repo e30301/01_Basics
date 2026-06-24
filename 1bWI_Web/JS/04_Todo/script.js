cb2491fb8d7fe0d908faaca4f8069dd5beb5737b
isaak vogel <isaak.vogel@student.htldornbirn.at>
2026-06-24 12:47:31 +0200

    design todo website | hide container on empty + bugfix

--- 1bWI_Web/JS/04_Todo/script.js
+++ 1bWI_Web/JS/04_Todo/script.js
@@ -11,11 +11,18 @@ todos.forEach((element) => {
     html += "<div>" + element.title + " - " + element.isDone + ' <button onclick="complete(' + element.id + ')">' + (element.isDone ? "uncomplete" : "complete") + '</button>' + ' <button onclick="deleteTodo(' + element.id + ')">delete</button></div>';
 });
 container.innerHTML = html;
+if (todos.length == 0) {
+     container.style.opacity = "0";
+} else {
+    container.style.opacity = "100";
+}
 }
 
 function addTodo() {
-    todos.push({id:id++,title:input.value,isDone:false})
-    update();
+    if (input.value !== "") {
+        todos.push({id:id++,title:input.value,isDone:false})
+        update();
+    }
 }
 
 function deleteTodo(id) {
@@ -31,3 +38,5 @@ function complete(id) {
     update()
 
 }
+
+update()
